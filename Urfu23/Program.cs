fc2a41d754862a28a0afe2c868136a6d6f9563f3
PopovKS <popov.k.s@ft-soft.ru>
2023-12-21 16:49:32 +0500

    Add MassTransit & EF.PostgreSQL

--- Urfu23/Program.cs
+++ Urfu23/Program.cs
@@ -1,9 +1,32 @@
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Urfu23.Infrastructure.Storage;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WebApplicationDbContext>(optionsBuilder =>
+{
+    var dbConnection = builder.Configuration.GetConnectionString("WebApplication");
+    optionsBuilder.UseNpgsql(dbConnection);
+});
+
+builder.Services.AddMassTransit(x =>
+{
+    //x.AddConsumer<SomeConsumer>();
+                
+    x.UsingRabbitMq((context, configurator) =>
+    {
+        configurator.Host(builder.Configuration.GetConnectionString("RabbitMq"));
+                    
+        configurator.ConfigureEndpoints(context);
+    });
+
+});
 
 var app = builder.Build();
 
